7df350b82e5a558768a6cfdd5812fc7e6c8b57fa
Jethro Glaudin <jethroglaudin.student@careerdevs.com>
2019-05-03 23:07:47 +0000

    updated server.js and package.json

--- server.js
+++ server.js
@@ -1,23 +1,15 @@
 const express = require("express");
-// const fruits = require('./data');
 // const words = require('./spellcheck');
-const wordData = require('./words_dictionary');
 const dictionary = require('./dictionary'); 
 const app = express();
-const port =  8080;
+const port = process.env.Port || 8080;
 // process.env.Port grabs its default port || 5000 checks if the first argument exists if not it uses 5000//
 const bodyParser = require('body-parser');
-// user//
-// app.get('/', function (req, res){
-// const userName = req.query.user;
-// res.send(`Hello ${userName}!`);
-// });
-// dictionary//
 app.use(bodyParser.json());
 app.use("/", express.static('public'));
 
 app.get('/api', function (req, res){
-    console.log(req.query.word)
+    console.log(req.query.word);
 const word = req.query.word;
 let found = `<div class="found">${word} was succesfully found in the dictionary</div>`;
 let missing = `<div class="missing">No Matches Found for ${word}</div>`;
